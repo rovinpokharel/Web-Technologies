1a9b82e957f5809007ffc4bfc6d6e8bffbe3bd45
Rovin <abhishek@bajratechnologies.com>
2023-01-18 09:17:15 +0545

    Integrated JS in HTML

---
+++ Week 9/js/script.js
@@ -0,0 +1,7 @@
+// alert("Don't make noise in class");
+console.log("Hello World!!!");
+console.log(document.getElementById("paragraph").innerHTML);
+document.getElementById("paragraph").innerHTML="<h1>Hello World!!</h1>";
+// document.getElementById("paragraph").style.color="purple";
+// document.getElementById("paragraph").style.background="yellow";
+document.getElementById("paragraph").style.cssText="color:purple; background:aqua;";
