ce7e6dfa31e99bc46dbe6fe698a2746e9337b400
Rovin <abhishek@bajratechnologies.com>
2023-01-18 09:28:33 +0545

    Added week 9 files

--- Week 9/js/script.js
+++ Week 9/js/script.js
@@ -5,3 +5,16 @@ document.getElementById("paragraph").innerHTML="<h1>Hello World!!</h1>";
 // document.getElementById("paragraph").style.color="purple";
 // document.getElementById("paragraph").style.background="yellow";
 document.getElementById("paragraph").style.cssText="color:purple; background:aqua;";
+
+var age = 10;
+age = 13;
+const age1 = 10;
+// age1 = 15;
+let age2 = 10;
+age2 = 20;
+var name = "Rovin";
+
+console.log(age);
+console.log(age1);
+console.log(age2);
+console.log(name);
